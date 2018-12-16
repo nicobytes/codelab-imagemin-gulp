7eb0afcff15dd26c5745fec9d394e3d1fffb5303
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-12-16 18:07:22 -0400

    🚧 work in plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,14 @@
 
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const mozjpeg = require('imagemin-mozjpeg');
 	 
 gulp.task('imagemin', () => {
   gulp.src('src/assets/imgs/*')
-  .pipe(imagemin())
+  .pipe(imagemin([
+    pngquant({quality: '50'}),
+    mozjpeg({quality: '50'})
+  ]))
   .pipe(gulp.dest('src/assets/imgs/'));
 });
