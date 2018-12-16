1e551147280482c33bd60e4f6397a85243bb571e
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-12-16 17:47:49 -0400

    🚧 include proccess for imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 //SPDX-License-Identifier: Apache-2.0
 
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 	 
-gulp.task('default', () => {
-  //Add tasks here
+gulp.task('imagemin', () => {
+  gulp.src('src/assets/imgs/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('src/assets/imgs/'));
 });
