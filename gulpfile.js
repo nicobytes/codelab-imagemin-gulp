003ba60ec91cb5471d6af4914042780f29b8675c
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-12-19 17:59:37 -0500

    🍱 optmize assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@
 //SPDX-License-Identifier: Apache-2.0
 
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const mozjpeg = require('imagemin-mozjpeg');
 	 
-gulp.task('default', () => {
-  //Add tasks here
+gulp.task('imagemin', () => {
+  gulp.src('src/assets/imgs/*')
+  .pipe(imagemin([
+    pngquant({quality: '50'}),
+    mozjpeg({quality: '50'})
+  ]))
+  .pipe(gulp.dest('src/assets/imgs/'))
 });
